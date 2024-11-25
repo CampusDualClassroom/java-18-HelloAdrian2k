b8c81218b28915941ab3bade09a26e7f8fe243f0
Adrián <mz.adriaan@gmail.com>
2024-11-25 14:15:37 +0100

    Ejercicio 18 finalizado.

--- src/main/java/com/campusdual/classroom/Exercise18.java
+++ src/main/java/com/campusdual/classroom/Exercise18.java
@@ -1,8 +1,25 @@
 package com.campusdual.classroom;
 
+import static com.campusdual.util.Utils.*;
+
 public class Exercise18 {
 
-	public static void main(String[] args) {
+	public static int[] createAndInitializeArray(int n){
+		int[] arrayTemplate = new int[n];
+		for(int i = 0; i < n; i++){
+			arrayTemplate[i] = i + 1;
+		}
+		return arrayTemplate;
+	}
 
+	public static void  showInlineArray(int[] a){
+		for(int i = 0; i < a.length; i++){
+			System.out.print((i != a.length - 1) ? a[i] + " " : a[i]);
+		}
+	}
+
+	public static void main(String[] args) {
+		System.out.print("Introduzca el número de elementos:\t");
+		showInlineArray(createAndInitializeArray(integer()));
 	}
 }
