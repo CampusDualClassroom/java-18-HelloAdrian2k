9962e2b3d0669b3cf4a04e88fd01b061f2231d46
Adrián <mz.adriaan@gmail.com>
2024-11-25 14:15:37 +0100

    Ejercicio 18 finalizado.

--- src/main/java/com/campusdual/classroom/Exercise18.java
+++ src/main/java/com/campusdual/classroom/Exercise18.java
@@ -4,22 +4,22 @@ import static com.campusdual.util.Utils.integer;
 
 public class Exercise18 {
 
-	public static int[] createAndInitializeArray(int n){
-		int[] arrayTemplate = new int[n];
-		for(int i = 0; i < n; i++){
-			arrayTemplate[i] = i + 1;
-		}
-		return arrayTemplate;
-	}
+    public static int[] createAndInitializeArray(int n) {
+        int[] arrayTemplate = new int[n];
+        for (int i = 0; i < n; i++) {
+            arrayTemplate[i] = i + 1;
+        }
+        return arrayTemplate;
+    }
 
-	public static void  showInlineArray(int[] a){
-		for(int i = 0; i < a.length; i++){
-			System.out.print((i != a.length - 1) ? a[i] + " " : a[i]);
-		}
-	}
+    public static void showInlineArray(int[] a) {
+        for (int i = 0; i < a.length; i++) {
+            System.out.print((i != a.length - 1) ? a[i] + " " : a[i]);
+        }
+    }
 
-	public static void main(String[] args) {
-		System.out.print("Introduzca el número de elementos:\t");
-		showInlineArray(createAndInitializeArray(integer()));
-	}
+    public static void main(String[] args) {
+        System.out.print("Introduzca el número de elementos:\t");
+        showInlineArray(createAndInitializeArray(integer()));
+    }
 }
